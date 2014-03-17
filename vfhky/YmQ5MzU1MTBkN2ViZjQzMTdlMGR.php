9be8d82ea0770473023a2ee4c3dfa594fc0627c0
vfhky <vfhky@qq.com>
2014-03-17 20:13:43 +0800

    优化淘宝IP库的API接口调用

--- vfhky/YmQ5MzU1MTBkN2ViZjQzMTdlMGR.php
+++ vfhky/YmQ5MzU1MTBkN2ViZjQzMTdlMGR.php
@@ -2,19 +2,26 @@
 	if (empty($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'], $_SERVER['HTTP_HOST']) === false) {
 		exit;
 	}
-	$c572bd935510d7ebf4317e0de91994db71979d35a = trim($_GET['GhpcyBpcy94db71979d35aBhIGV4YW1wbGUd7ebf4317e0de919']);	
-	if(!filter_var($c572bd935510d7ebf4317e0de91994db71979d35a, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+	$request_urls = trim($_GET['GhpcyBpcy94db71979d35aBhIGV4YW1wbGUd7ebf4317e0de919']);	
+	if(!filter_var($request_urls, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
 		exit;
 	}
-	$ip=explode('.',$c572bd935510d7ebf4317e0de91994db71979d35a);  
+	$ip=explode('.',$request_urls);  
 	for($i=0;$i<count($ip);$i++)  
-	{  
+	{
 		if($ip[$i]>255){  
-			exit;  
+			exit;
 		}
 	}	
-	$c572bd935510d7ebf4317e0de91994db71979d35a = trim($c572bd935510d7ebf4317e0de91994db71979d35a);
-	$url572bd935510d7ebf4317e0de91994db71979d35a = 'http://ip.taobao.com/service/getIpInfo.php?ip='.$c572bd935510d7ebf4317e0de91994db71979d35a; 
-	$data = file_get_contents($url572bd935510d7ebf4317e0de91994db71979d35a); //调用淘宝接口获取信息 
-	echo $data;
+	$request_urls = trim($request_urls);
+	$post_urls = 'http://ip.taobao.com/service/getIpInfo.php?ip='.$request_urls;
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $post_urls);
+	curl_setopt($ch, CURLOPT_HEADER, FALSE);
+	curl_setopt($ch, CURLOPT_POST,FALSE);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+	$strRes = curl_exec($ch);
+	curl_close($ch);
+	echo $strRes ;
 ?>
